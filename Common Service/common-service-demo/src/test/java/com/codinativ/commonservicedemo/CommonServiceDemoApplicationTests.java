06c3b8620b346453956ef889448c37af489555fc
Philip Niron Nithianandan <70926504+Niron-code@users.noreply.github.com>
2022-04-11 13:25:08 +0530

    Common Service Commit

---
+++ Common Service/common-service-demo/src/test/java/com/codinativ/commonservicedemo/CommonServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codinativ.commonservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
